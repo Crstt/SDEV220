b031562451b8e3f3c018dd1bdaf37364a415c628
Crstt <matteo598.catalano@gmail.com>
2023-04-28 12:12:49 -0400

    M07 Case study

--- Django/M04/blog/urls.py
+++ Django/M04/blog/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth import views as vAuth
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('accounts/login/', vAuth.LoginView.as_view(), name='login'),
+    path('accounts/logout/', vAuth.LogoutView.as_view(next_page='/'), name='logout'),
 ]
